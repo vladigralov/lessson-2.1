9249e46dfc89e58de5890fa8be2e1300cf5e38f2
vladigralov <53748671+vladigralov@users.noreply.github.com>
2019-08-12 11:39:13 +0300

    add red green squares

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let canvas = document.getElementById('sandbox'),
 	context = canvas.getContext("2d"), 
-	square, circle;
-square = new Path2D();
-square.moveTo(50, 50);
-square.lineTo(250, 50);
-square.lineTo(250, 250);
-square.lineTo(50, 250);
-square.lineTo(50, 50);
+	line = new Path2D();
 
-context.stroke(square);
+line.moveTo(0,0);
+line.lineTo(300, 300);
 
-circle = new Path2D();
-circle.arc(150, 150, 100, 0, 2*Math.PI);
-context.fill(circle);
+context.lineWidth = 5;
+context.stroke(line);
+
+context.fillStyle = "red";
+context.fillRect(0,0,200,200);
+
+context.fillStyle = "rgba(0, 255, 0, 0.5)";
+context.fillRect(100,100,300,300);
 
 
+
+
+	
